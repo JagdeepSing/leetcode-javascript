8c25f9553918c73df447d3f5c325deb750125f2b
Jagdeep Singh <jagdeeeeep@gmail.com>
2019-09-13 14:38:23 -0700

    remove console.log

--- problems/graph/shortest-path-alternating-colors/shortest-alternating-path.js
+++ problems/graph/shortest-path-alternating-colors/shortest-alternating-path.js
@@ -8,7 +8,6 @@ class Node {
   }
 }
 
-console.log(shortestAlternatingPaths(6, [[2,3],[0,5],[3,1],[5,0],[3,0],[0,0]], [[1,2],[3,2],[1,0],[2,2],[5,2]]));
 // Shortest Path with Alternating Colors
 
 function shortestAlternatingPaths(n, red_edges, blue_edges) {
